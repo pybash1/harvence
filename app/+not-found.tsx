696f8df1ddae643402626f972cb52ede9f9bb498
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-12-04 00:29:07 +0530

    feat(not found page): improved the not found page to match the color palette
    also added some basic info and a back to home button

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,15 +1,25 @@
-import { Link, Stack } from 'expo-router';
+import { Link, Stack } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      {/* <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+      <Stack.Screen
+        options={{
+          title: "Oops!",
+          headerStyle: { backgroundColor: "#3355ff" },
+          headerTintColor: "#ffffff",
+        }}
+      />
+      <View className="h-full bg-primary flex items-center justify-center gap-6">
+        <Text className="text-xl text-center text-white">This view does not exist!</Text>
+        <Link
+          href="/"
+          className="text-white font-bold bg-black py-2 px-6 rounded-full text-3xl transition ease-in-out duration-300 font-plainBold active:bg-yellow-400 active:text-black active:scale-150"
+        >
+          Go Home
         </Link>
-      </ThemedView> */}
+      </View>
     </>
   );
 }
