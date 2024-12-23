5148ba358384907a2fd5b6b27f798f3f28b08d1f
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-12-23 17:00:37 +0530

    fix: fixed the scale getting stuck issue

--- app/index.tsx
+++ app/index.tsx
@@ -167,13 +167,13 @@ export default function HomeScreen() {
           <View className="h-full flex items-center justify-center gap-4">
             <Link
               href="/scan"
-              className="text-white font-bold bg-black py-2 px-6 rounded-full text-3xl transition ease-in-out duration-300 font-plainBold active:bg-yellow-400 active:text-black active:scale-150"
+              className="text-white font-bold bg-black py-2 px-6 rounded-full text-3xl transition ease-in-out duration-300 font-plainBold active:bg-yellow-400 active:text-black"
             >
               Scan
             </Link>
             <Link
               href="/history"
-              className="text-white font-bold bg-black py-2 px-6 rounded-full text-3xl transition ease-in-out duration-300 font-plainBold active:bg-yellow-400 active:text-black active:scale-150"
+              className="text-white font-bold bg-black py-2 px-6 rounded-full text-3xl transition ease-in-out duration-300 font-plainBold active:bg-yellow-400 active:text-black"
             >
               History
             </Link>
