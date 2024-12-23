41f5abb5dc2311ea6de8ead6dd1f315f3f11e4bf
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-12-23 16:04:20 +0530

    refactor: fixed and updated basic configs and package versions

--- app/+html.tsx
+++ app/+html.tsx
@@ -18,22 +18,9 @@ export default function Root({ children }: PropsWithChildren) {
           However, body scrolling is often nice to have for mobile web. If you want to enable it, remove this line.
         */}
         <ScrollViewStyleReset />
-
-        {/* Using raw CSS styles as an escape-hatch to ensure the background color never flickers in dark-mode. */}
-        <style dangerouslySetInnerHTML={{ __html: responsiveBackground }} />
         {/* Add any additional <head> elements that you want globally available on web... */}
       </head>
       <body>{children}</body>
     </html>
   );
 }
-
-const responsiveBackground = `
-body {
-  background-color: #fff;
-}
-@media (prefers-color-scheme: dark) {
-  body {
-    background-color: #000;
-  }
-}`;
